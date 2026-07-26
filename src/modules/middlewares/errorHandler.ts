db00fd1cad0be95e290a3f7b287d0df9d3d4c9dc
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2026-07-26 12:52:18 -0300

    feat: update errorHandler

--- src/modules/middlewares/errorHandler.ts
+++ src/modules/middlewares/errorHandler.ts
@@ -1,8 +1,18 @@
-import { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 import AppError from "@errors/AppError";
+import ValidationError from "@errors/ValidationError";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error('API ERROR', err);
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    if (err instanceof ValidationError) {
+        return res.status(err.httpStatusCode).json({
+            message: err.message,
+            errores: err.errores,
+        });
+    }
 
     if (err instanceof AppError) {
         return res.status(err.httpStatusCode).json({
@@ -10,6 +20,5 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
         });
     }
 
-    // Error no controlado / no esperado)
     return res.status(500).json({ error: "Error interno del servidor" });
 };
