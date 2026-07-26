ec51ddedbe3e198f15ec4514578063a1a413a1b8
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2026-07-26 01:27:15 -0300

    feat: add generics in IRepository interface

--- src/interfaces/IRepository.ts
+++ src/interfaces/IRepository.ts
@@ -1,7 +1,7 @@
-export interface IRepository {
-    crear(entidad: any): Promise<any>;
-    obtenerPorId(id: string): Promise<any>;
-    obtenerTodos(): Promise<any[]>;
-    actualizar(id: string, entidad: any): Promise<any>;
-    eliminar(id: string): Promise<any>;
+export interface IRepository<TEntity, TCrearDTO, TActualizarDTO = Partial<TCrearDTO>> {
+    crear(data: TCrearDTO): Promise<TEntity>;
+    obtenerPorId(id: number): Promise<TEntity>;
+    obtenerTodos(): Promise<TEntity[]>;
+    actualizar(id: number, data: TActualizarDTO): Promise<TEntity>;
+    eliminar(id: number): Promise<TEntity>;
 }
