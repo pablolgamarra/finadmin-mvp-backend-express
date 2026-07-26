9368f725311c1ac6f6e5868a06f60d06db42ca71
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2026-07-26 12:52:03 -0300

    fix: add log of error in console of RepositoryError

--- src/errors/RepositoryError.ts
+++ src/errors/RepositoryError.ts
@@ -3,5 +3,6 @@ import AppError from "@errors/AppError";
 export default class RepositoryError extends AppError {
     constructor(message: string, public readonly cause?: unknown) {
         super(message, 500, false);
+        cause && console.error(cause);
     }
 }
