05d5883bda78657697fc7b8e74d7239b5d6e10fc
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2026-07-26 12:51:42 -0300

    fix: replace name of import in prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "@prismaGenerated";
+import { PrismaClient } from "@prismaGeneratedClient";
 
 const connectionString = `file:./dev.db`;
 
