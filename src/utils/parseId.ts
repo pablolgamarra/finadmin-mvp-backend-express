0756fa2852bff177f023083f01e3b1f677df8528
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2026-07-28 17:23:52 -0300

    feat: add parseId utility function

---
+++ src/utils/parseId.ts
@@ -0,0 +1,13 @@
+import InvalidDtoError from "@errors/InvalidDtoError";
+import InvalidQueryError from "@errors/InvalidQueryError";
+
+export function parseIdQuery(idString: string | undefined): number {
+    if (!idString) {
+        throw new InvalidQueryError('Parametro ID no encontrado');
+    }
+    const id = Number(idString);
+    if (Number.isNaN(id)) {
+        throw new InvalidDtoError('Id no válido');
+    }
+    return id;
+}
