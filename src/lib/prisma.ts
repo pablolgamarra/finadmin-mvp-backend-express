2c26015aa687711fa62347ef6dd9fd7618d094f5
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2026-08-02 21:29:10 -0300

    feat: use dbcheck on prisma client connection

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prismaGeneratedClient";
+import { firstCheck } from "@utils/firstDBCheck";
 
 const connectionString = `file:./dev.db`;
 
 const adapter = new PrismaBetterSqlite3({ url: connectionString });
 const prisma = new PrismaClient({ adapter });
+
+firstCheck(prisma);
 
 export { prisma };
